2bd4721ad6661694997866fa719bc2c5387c5aa3
jaccohub <jacoblodenius@gmail.com>
2017-04-02 01:55:26 +0200

    test

--- src/main/java/se/simjarr/PoeApiWrapperApplication.java
+++ src/main/java/se/simjarr/PoeApiWrapperApplication.java
@@ -5,10 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import se.simjarr.model.ApiDataFetch;
 
+@Controller
 @SpringBootApplication
 public class PoeApiWrapperApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoeApiWrapperApplication.class, args);
