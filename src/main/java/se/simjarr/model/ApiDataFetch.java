41cb955f66aa40c4f443fb3ca60bf22c32793381
jaccohub <jacoblodenius@gmail.com>
2017-04-02 20:33:51 +0200

    change_id bugfix

--- src/main/java/se/simjarr/model/ApiDataFetch.java
+++ src/main/java/se/simjarr/model/ApiDataFetch.java
@@ -27,8 +27,7 @@ public class ApiDataFetch {
         this.userRepository = userRepository;
         this.nextChangeIdRepository = nextChangeIdRepository;
 
-        nextChangeId = new NextChangeId("57570536-60886358-57044206-66343893-61605538");
-        //nextChangeId = nextChangeIdRepository.findOne("next_id");
+        nextChangeId = nextChangeIdRepository.findOne("next_id");
     }
 
     @Scheduled(fixedDelay = 10000)
